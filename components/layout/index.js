d40135af0a70eb366f62b53dfa5fe6a0fbf158f2
Alex Lin <alex.lin9991@gmail.com>
2022-08-30 18:23:32 +1000

    change loader to icon

--- components/layout/index.js
+++ components/layout/index.js
@@ -2,12 +2,14 @@ import Header from "../sections/Header";
 import Footer from "../sections/Footer";
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
+import Image from "next/image";
+import LoadIcon from "../../assets/icon.png";
 
 const Layout = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 2000);
+    setTimeout(() => setIsLoading(false), 3000);
   }, []);
 
   return (
@@ -16,11 +18,11 @@ const Layout = ({ children }) => {
         <div className="fixed w-screen h-screen bg-white z-20 flex items-center justify-center">
           <motion.h2
             animate={{
-              opacity: [0, 1, 0],
+              opacity: [0, 1, 0, 1, 0],
             }}
-            transition={{ duration: 2 }}
+            transition={{ duration: 3 }}
           >
-            Loading...
+            <Image src={LoadIcon}></Image>
           </motion.h2>
         </div>
       ) : (
--- components/sections/About/index.js
+++ components/sections/About/index.js
@@ -6,11 +6,10 @@ import uiStaticTags from "../../../assets/uiTagsStatic.png";
 const About = () => {
   return (
     <div className="full-width-section mt-48" id="about">
-      <Row ss={1} es={7} sm={2} em={10} sl={3} el={13} className="content-box">
+      <Row className="content-box px-[25%] py-24 rounded-[3rem] bg-[#000000] text-white">
         <div className="flex flex-col">
-          <h2 className="text-center text-text-dark-grey mb-16">
-            Get to know me
-          </h2>
+          <h2 className="text-center mb-16">Get to know me</h2>
+
           <div className="flex flex-col items-start gap-8 md:flex-row justify-between">
             <div className="flex flex-col md:w-[50%] md:min-w-[16rem] gap-16">
               <div className="flex flex-col gap-4">
